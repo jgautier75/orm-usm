44b5f90c67ace0f5adfe0f46d291ea20f638c019
jga75 <jerome.gautier79@orange.fr>
2024-01-15 14:21:56 +0100

    Init Db: Manage idempotency

--- db-migration/scripts/init.sh
+++ db-migration/scripts/init.sh
@@ -10,13 +10,12 @@ echo "P_DBAPASS=$P_DBAPASS"
 echo "P_APPUSER=$P_APPUSER"
 echo "P_APPPASS=$P_APPPASS"
 DEFAULT_DB="postgres"
-
-#./init.sh 192.168.1.15 5432 postgres posgres orm-test dba_user dba_pass app_user app_pass
+DEFAULT_SCHEMA="public"
 
 export PGPASSWORD=${P_PGPASS}
 
+# Create databse if not exist
 DB_EXISTS=$(psql -h $P_PGHOST -U $P_PGUSER -d $DEFAULT_DB -bc "SELECT 1 FROM pg_database WHERE datname='$P_DBNAME'")
-
 TEST_EXISTS="1 row"
 CLEANED=${DB_EXISTS//[$'\t\r\n']} && CLEANED=${CLEANED%%*( )}
 EXISTS=$(echo "$CLEANED" | grep "$TEST_EXISTS" | wc -l)
@@ -28,6 +27,7 @@ else
     psql postgresql://${P_PGUSER}:${P_PGPASS}@${P_PGHOST}:${P_PGPORT} -f db_create.sql
 fi
 
+# Create user accounts if not exist
 USR_DBA_EXISTS=$(psql -h $P_PGHOST -U $P_PGUSER -d $DEFAULT_DB -bc "select 1 from pg_roles where rolname = '$P_DBAUSER'")
 CLEANED=${USR_DBA_EXISTS//[$'\t\r\n']} && CLEANED=${CLEANED%%*( )}
 EXISTS=$(echo "$CLEANED" | grep "$TEST_EXISTS" | wc -l)
@@ -36,7 +36,17 @@ if [ "$EXISTS" == "1" ]; then
 else
     echo "User account [$P_DBAUSER] does not exist"
     sed -e "s/DBNAME/${P_DBNAME}/g" -e "s/DBAUSER/${P_DBAUSER}/g" -e "s/DBAPASS/${P_DBAPASS}/g" -e "s/APPUSER/${P_APPUSER}/g" -e "s/APPPASS/${P_APPPASS}/g" accounts_setup.sql > accounts.sql 
-    psql postgresql://${P_PGUSER}:${P_PGPASS}@${P_PGHOST}:${P_PGPORT} -f accounts.sql
+    psql -1 -b postgresql://${P_PGUSER}:${P_PGPASS}@${P_PGHOST}:${P_PGPORT} -f accounts.sql
+    echo "Grants for dba user"
+    sed -e "s/LSCHEMA/${DEFAULT_SCHEMA}/g" -e "s/DBAUSER/${P_DBAUSER}/g"  grants_dba_template.sql > grants_dba.sql 
+    psql -1 -b postgresql://${P_PGUSER}:${P_PGPASS}@${P_PGHOST}:${P_PGPORT}/${P_DBNAME} -f grants_dba.sql
 fi
 
-java -jar db-migration.jar --classpath=db-migration.jar --driver=org.postgresql.Driver --url="jdbc:postgresql://${P_PGHOST}:${P_PGPORT}/${P_DBNAME}" --changeLogFile="postgresql/changelogs.xml" --username="${P_DBAUSER}" --password="${P_DBAPASS}" --logLevel=info  update
+# Liquibase update
+sed -e "s/HOST/${P_PGHOST}/g" -e "s/PORT/${P_PGPORT}/g" -e "s/DBNAME/${P_DBNAME}/g" -e "s/LSCHEMA/${DEFAULT_SCHEMA}/g" -e "s/DBAUSER/${P_DBAUSER}/g" -e "s/DBAPASS/${P_DBAPASS}/g" liquibase_template.properties > liquibase.properties
+java -jar db-migration.jar --defaultsFile=liquibase.properties update
+
+# Grants for app user
+sed -e "s/LSCHEMA/${DEFAULT_SCHEMA}/g" -e "s/APPUSER/${P_APPUSER}/g" grants_template.sql > grants.sql 
+echo "Execute grants"
+psql -1 -b postgresql://${P_PGUSER}:${P_PGPASS}@${P_PGHOST}:${P_PGPORT} -f grants.sql
